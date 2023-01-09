8be0a0c3051937d99a861cef7570c7bd19601cce
Enantiodromis <georgerebradley@gmail.com>
2023-01-09 14:13:30 +0100

    Updates with wavelength analysis

--- main.py
+++ main.py
@@ -10,53 +10,19 @@ from data_methods import *
 from protocol import *
 import scipy.signal as sp
 from protocol_updated import *
+from wavelength_analysis import run_wavelength_analysis
 
-#['2','30','31','11']
-#clean_IICP = load_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Cleaned_data_V4/IMPROVED_V4_line_threshold_0.0025_threshold_1.5_iicp_data_cleaned_9.csv")
-#clean_810_distal = load_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Cleaned_data_V4/IMPROVED_V4_line_threshold_0.0025_threshold_1.5_DISTAL_810_nicp_data_cleaned_9.csv")
-#clean_810_proximal = load_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Cleaned_data_V4/IMPROVED_V4_line_threshold_0.0025_threshold_1.5_PROXIMAL_810_nicp_data_cleaned_9.csv")
+dis_770 = pd.read_csv("Features_All_Wav/770/distal_features.csv")
+dis_810 = pd.read_csv("Features_All_Wav/810/distal_features.csv")
+dis_850 = pd.read_csv("Features_All_Wav/850/distal_features.csv")
+dis_880 = pd.read_csv("Features_All_Wav/880/distal_features.csv")
 
-clean_IICP = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/IICP/Joint_Data_IICP_16_12_22.csv")
-clean_IICP = clean_IICP.drop(columns = ['19','9'], axis=1)
+prox_770 = pd.read_csv("Features_All_Wav/770/proximal_features.csv")
+prox_810 = pd.read_csv("Features_All_Wav/810/proximal_features.csv")
+prox_850 = pd.read_csv("Features_All_Wav/850/proximal_features.csv")
+prox_880 = pd.read_csv("Features_All_Wav/880/proximal_features.csv")
 
-"""# 770 wavelength
-clean_770_distal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/DIS_770_Joint_Data_IICP_16_12_22.csv")
-clean_770_proximal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/PROX_770_Joint_Data_IICP_16_12_22.csv")
+run_wavelength_analysis(dis_only = True, dis_770=dis_770, dis_810=dis_810, dis_850=dis_850, dis_880=dis_880)
+run_wavelength_analysis(prox_only = True, prox_770=prox_770, prox_810=prox_810, prox_850=prox_850, prox_880=prox_880)
 
-clean_770_distal = clean_770_distal.drop(columns = ['19','9'], axis=1)
-clean_770_proximal = clean_770_proximal.drop(columns = ['19','9'], axis=1)
-
-clean_IICP, clean_770_distal, clean_770_proximal = remove_values(clean_IICP, clean_770_distal, clean_770_proximal, upper=60, lower=0)
-run_feature_extraction(6000, clean_770_distal, clean_770_proximal, clean_IICP, "Features_All_Wav/770", debug = 0)"""
-
-"""# 810 wavelength
-clean_810_distal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/DIS_810_Joint_Data_IICP_16_12_22.csv")
-clean_810_proximal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/PROX_810_Joint_Data_IICP_16_12_22.csv")
-
-clean_810_distal = clean_810_distal.drop(columns = ['19','9'], axis=1)
-clean_810_proximal = clean_810_proximal.drop(columns = ['19','9'], axis=1)
-
-clean_IICP, clean_810_distal, clean_810_proximal = remove_values(clean_IICP, clean_810_distal, clean_810_proximal, upper=60, lower=0)
-run_feature_extraction(6000, clean_810_distal, clean_810_proximal, clean_IICP, "Features_All_Wav/810", debug = 0)
-
-# 850 wavelength
-clean_850_distal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/DIS_850_Joint_Data_IICP_16_12_22.csv")
-clean_850_proximal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/PROX_850_Joint_Data_IICP_16_12_22.csv")
-
-clean_850_distal = clean_850_distal.drop(columns = ['19','9'], axis=1)
-clean_850_proximal = clean_850_proximal.drop(columns = ['19','9'], axis=1)
-
-clean_IICP, clean_850_distal, clean_850_proximal = remove_values(clean_IICP, clean_850_distal, clean_850_proximal, upper=60, lower=0)
-run_feature_extraction(6000, clean_850_distal, clean_850_proximal, clean_IICP, "Features_All_Wav/850", debug = 0)
-
-# 880 wavelength
-clean_880_distal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/DIS_880_Joint_Data_IICP_16_12_22.csv")
-clean_880_proximal = pd.read_csv("C:/Users/k20113376/Documents/Clinical Trial/NEW_Code_Data/Data/Data_Cleaned/Data_All_Wav_16_12_22/Joint Data/NICP/PROX_880_Joint_Data_IICP_16_12_22.csv")
-
-clean_880_distal = clean_880_distal.drop(columns = ['19','9'], axis=1)
-clean_880_proximal = clean_880_proximal.drop(columns = ['19','9'], axis=1)
-
-clean_IICP, clean_880_distal, clean_880_proximal = remove_values(clean_IICP, clean_880_distal, clean_880_proximal, upper=60, lower=0)
-run_feature_extraction(6000, clean_880_distal, clean_880_proximal, clean_IICP, "Features_All_Wav/880", debug = 0)
-"""
 
