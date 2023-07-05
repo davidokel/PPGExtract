9d513f9630bcde9fd4eada337af3407991b713ef
Enantiodromis <georgerebradley@gmail.com>
2023-07-05 06:58:23 +0100

    protocol update

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import pandas as pd
-from protocols import extraction_protocol_v2, extraction_protocol_v3
+from protocols import extraction_protocol_v2
 import matplotlib.pyplot as plt
+import pickle as pkl
+from support_code.pulse_detection import get_pulses
 
-# Use this file to call upon other files and functions for execution
-test_dataset = pd.read_csv("Data/test_data.csv")[:6000]
-test_data = test_dataset["Data"][:6000]
-test_data = test_data * -1
+# Column names to keep 
+#columns_to_keep = ["1","10","14","15","16","17","18","19","20","22","23","25","26","27","28","29","36","38","4","5","6","7","8"]
 
-extraction_protocol_v2(test_dataset, test_data, 100, 6000, "test", visualise=True, debug=True, derivative=[1,2])
+#nicp_dataset = pd.read_pickle("D:\\Denoising\\NICP\\nICP_denoised_joint.pkl")
+#icp_dataset = pd.read_pickle("D:\\Denoising\\ICP\\ICP_denoised_joint.pkl")
+
+"""# Get index of column with name "22"
+index = nicp_dataset.columns.get_loc("27")
+
+# Get data from column 22 onwards and get the top 12000 rows
+nicp_dataset = nicp_dataset.iloc[:12000,index:]
+icp_dataset = icp_dataset.iloc[:12000,index:]"""
+
+#extraction_protocol_v2(nicp_dataset, icp_dataset, 100, 6000, visualise=False, debug=False, derivative=[1,2])
+
+# Load test_10_06_2023.csv
+test_dataset = pd.read_csv("test_10_06_2023.csv")
+data = test_dataset["Data"]
+
+# Iterate over the data in windows of 6000 samples
+for i in range(0, len(data), 6000):
+    # Get the data in the window
+    window = data[i:i+6000]
+    # Get the pulses in the window
+    pulses, peaks, troughs = get_pulses(list(window), fs=100, visualise=True, debug=True, z_score_threshold=3, z_score_detection=True)
 
 
--- protocols.py
+++ protocols.py
@@ -110,6 +110,96 @@ def extraction_protocol_v2(nicp_dataset, icp_dataset, fs, window_size, visualise
     joint_patient_features.to_pickle("D:\\Datasets\\Features\\joint_patient_features_" + now.strftime("%Y-%m-%d") + ".pkl")
 
 
+def extraction_protocol_v3(nicp_dataset, icp_dataset, fs, window_size, visualise = False, debug = False, z_score_threshold = 3, z_score_detection = True, derivative=[]):
+    # Check if window_size is 0 or negative
+    if window_size <= 0:
+        raise ValueError("window_size must be greater than 0")
+    
+    patient_features_df = pd.DataFrame()
+    joint_patient_features = pd.DataFrame()
+
+    # Iterate over the columns of the dataset
+    for column in nicp_dataset.columns:
+
+        # Check IF column is in the icp dataset raise error
+        if column not in icp_dataset.columns:
+            raise ValueError("Column " + column + " is not in the icp dataset")
+
+        # Check if the column exists in both datasets
+        nicp_data = nicp_dataset[column]
+        icp_data = icp_dataset[column]
+        # Drop the nan values
+        nicp_data = nicp_data.dropna()
+        icp_data = icp_data.dropna()
+
+        # Get 10 first rows of the data
+        nicp_data = nicp_data
+        icp_data = icp_data
+
+        # Check if the length of the data is the same
+        if len(nicp_data) != len(icp_data):
+            raise ValueError("Length ICP and NICP data is not the same: " + str(len(nicp_data)) + " vs " + str(len(icp_data)) + " for column " + column)
+        
+        # Calculate the total number of windows
+        num_windows = math.ceil(len(nicp_data)/window_size)
+
+        patient_features = {}
+
+        # Check if len(data) < window_size and not 0
+        if len(nicp_data) < window_size and len(nicp_data) > 0:
+            window_size = len(nicp_data)
+
+        num_windows = math.ceil(len(nicp_data)/window_size) # Calculating the number of windows the data will be split into
+        last_window_size = len(nicp_data) % window_size # Calculating the size of the last window
+
+        for window in range(num_windows):
+            start = window*window_size # Calculating the start index of the current window
+            end = start + window_size # Calculating the end index of the current window
+            
+            if window == num_windows - 1 and last_window_size > 0:
+                end = start + window_size + last_window_size # Calculating the end index of the last window
+
+            # +---------------------+   
+            # | DEFINING DATA CHUNK |
+            # +---------------------+
+            data_window = nicp_data[start:end] # Defining the data window
+            icp_window = icp_data[start:end] # Defining the icp window
+
+            # Print the current window
+            print("----------------------------------------------------")
+            print(str(window) + "/" + str(num_windows))
+            print("Column: " + str(column))
+            # Print the start and end of the window
+            print("Start: " + str(start) + " End: " + str(end))
+            # Print the total length of the data without nan values
+            print("Length of data: " + str(len(nicp_data.dropna())))
+            print("----------------------------------------------------")
+            print("")
+            
+            #################################################
+            # Extracting the pulses from the current window #
+            #################################################
+            peak_points, peaks, troughs = get_pulses(list(data_window), fs=fs, visualise=True, debug=debug, z_score_threshold=z_score_threshold, z_score_detection=z_score_detection)
+            features = get_features_v2(peak_points, fs, visualise=visualise, debug=debug)
+            patient_features[window] = features
+            
+            # Check if derivative is not empty
+            if derivative:
+                deriv_dictionary = {}
+                for i in derivative:
+                    #################################################
+                    # Extracting the pulses from the current window #
+                    #################################################
+                    peak_points, peaks, troughs = get_pulses(list(data_window), fs=fs, visualise=True, debug=debug, z_score_threshold=z_score_threshold, z_score_detection=z_score_detection, derivative=i)
+                    features = get_features_v2(peak_points, fs, visualise=visualise, debug=debug)
+                    # Change the name of each value in the dictionary to include the derivative
+                    features = {key + "_deriv_" + str(i): value for key, value in features.items()}
+                    deriv_dictionary.update(features)
+                patient_features[window].update(deriv_dictionary)
+            
+            patient_features[window]["mean_icp"] = np.mean(icp_window)
+            patient_features[window]["median_icp"] = np.median(icp_window)
+            patient_features[window]["column"] = column
 
 
             
