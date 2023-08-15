d16c219dae2e4f7d8da6cf27356bd1ae6bda7c40
Enantiodromis <georgerebradley@gmail.com>
2023-08-15 18:19:52 +0100

    code update

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 import pandas as pd
 from protocols import extraction_protocol_v2
-import matplotlib.pyplot as plt
-import pickle as pkl
 from support_code.pulse_detection import get_pulses
-
-# Column names to keep 
-#columns_to_keep = ["1","10","14","15","16","17","18","19","20","22","23","25","26","27","28","29","36","38","4","5","6","7","8"]
-
-#nicp_dataset = pd.read_pickle("D:\\Denoising\\NICP\\nICP_denoised_joint.pkl")
-#icp_dataset = pd.read_pickle("D:\\Denoising\\ICP\\ICP_denoised_joint.pkl")
-
-"""# Get index of column with name "22"
-index = nicp_dataset.columns.get_loc("27")
-
-# Get data from column 22 onwards and get the top 12000 rows
-nicp_dataset = nicp_dataset.iloc[:12000,index:]
-icp_dataset = icp_dataset.iloc[:12000,index:]"""
-
-#extraction_protocol_v2(nicp_dataset, icp_dataset, 100, 6000, visualise=False, debug=False, derivative=[1,2])
 
 # Load test_10_06_2023.csv
 test_dataset = pd.read_csv("test_10_06_2023.csv")
